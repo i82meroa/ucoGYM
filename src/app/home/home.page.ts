7ceb93b1b825775d647cff3ebda59af0fbdd5c07
Maria <mariagarciatrs@gmail.com>
2021-11-21 12:16:37 +0100

    Reconfiguración de tab1 a home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
 })
-export class Tab1Page {
+export class HomePage {
 
   constructor(private router: Router) {}
 
