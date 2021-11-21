f57ebec8893c9262931df45250d100a120c9b623
Maria <mariagarciatrs@gmail.com>
2021-11-21 20:52:41 +0100

    Home del administrador completo

--- src/app/home-admin/home-admin.page.ts
+++ src/app/home-admin/home-admin.page.ts
@@ -7,8 +7,18 @@ import { Router } from '@angular/router';
   styleUrls: ['home-admin.page.scss']
 })
 export class HomeAdminPage {
+  username: string;
 
   constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    this.getUsername();
+  }
+
+  getUsername() {
+      this.username = window.localStorage.getItem('userUsername');
+      console.log("Username is: " + this.username);
+  }
 
   logout(){
     window.localStorage.clear();
