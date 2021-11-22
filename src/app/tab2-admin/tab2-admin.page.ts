fe40d7aa626ee5d025e1ebb3552bea1ebb85a3d5
Antonio Jesús <i82ropia@uco.es>
2021-11-22 15:55:18 +0100

    añadir rutina

--- src/app/tab2-admin/tab2-admin.page.ts
+++ src/app/tab2-admin/tab2-admin.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set,} from 'firebase/database';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-tab2-admin',
@@ -6,7 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2-admin.page.scss']
 })
 export class Tab2AdminPage {
+  
+ nombreRutina:string;
+ ejercicios:any[] = [];
+ ejercicio:any = {};
 
-  constructor() {}
+
+       
+
+  constructor(private router: Router) { }
+
+
+addExercise()
+{
+  this.ejercicios.push(this.ejercicio);
+  this.ejercicio = {};
 
 }
+  SaveRoutine(){
+
+    const db = getDatabase();
+    const referencia =  set(ref( db,' rutinas/' + this.nombreRutina), {
+      nombreEjercicio: this.ejercicio.nombreEjercicio,
+      seriesRep: this.ejercicio.seriesRepeticiones
+    }); 
+
+    
+  }
+
+} 
+  
+  
