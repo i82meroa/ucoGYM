388375b4f1c47c3d1013a998300856e7b50f49b4
Juanfran <juanfranromero01@gmail.com>
2021-11-23 17:32:36 +0100

    Añadir rutina

--- src/app/tab2-admin/tab2-admin.page.ts
+++ src/app/tab2-admin/tab2-admin.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get, child, set } from 'firebase/database';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2-admin',
   templateUrl: 'tab2-admin.page.html',
   styleUrls: ['tab2-admin.page.scss']
 })
+
 export class Tab2AdminPage {
+  nombreRutina: string;
+  ejercicios: any=[];
+  ejercicio: any= {};
 
-  constructor() {}
+  constructor(private router: Router) { }
 
+  addExercise(){
+    console.log( this.ejercicio);
+    this.ejercicios.push(this.ejercicio);
+    console.log(this.ejercicios);
+    this.ejercicio = {};
+  };
+
+  saveRoutine(){
+    const app = initializeApp(environment.firebase);
+    const db = getDatabase(app);
+    console.log(this.nombreRutina);
+    console.log(this.ejercicios);
+    set(ref(db, 'rutinas/' + this.nombreRutina), this.ejercicios);
+  };
+
+  logout(){
+    window.localStorage.clear();
+    this.router.navigateByUrl('/login');
+  }
 }
