b8ea21cc49da3c3029ede4e8d26b6365ae3a2d23
Javi <javiergm615@gmail.com>
2021-11-23 16:54:51 +0100

    añadir pesaje

--- src/app/tab1-admin/tab1-admin.page.ts
+++ src/app/tab1-admin/tab1-admin.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get, child, set } from "firebase/database";
+import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,8 +10,76 @@ import { Router } from '@angular/router';
   styleUrls: ['tab1-admin.page.scss']
 })
 export class Tab1AdminPage {
+  username: String;
+  fechaPesaje: Date= new Date();
+  peso: number;
+  imc: number;
+  metabolismoBasal: number;
+  grasa: number;
+  masaMuscular: number;
+  porcentajeAgua: number;
+
 
   constructor(private router: Router) {}
+
+  anadirPesaje(_user, _fechaPesaje, _peso, _imc, _metabolismoBasal, _grasa, _masaMuscular, _porcentajeAgua) {
+    
+    console.log('Bindeo correcto. Username: ',this.username,' fecha de Pesaje: ', this.fechaPesaje, ' Peso: ', this.peso, ' imc: ', this.imc, 
+                ' Metabolismo Basal: ', this.metabolismoBasal, ' Grasa: ', this.grasa, ' Masa Muscular: ', this.masaMuscular, ' Porcentaje de agua: ', this.porcentajeAgua);
+    
+    const app = initializeApp(environment.firebase);
+    const db = getDatabase(app);
+    const dbRef = ref(getDatabase(app));
+    const referencia= get(child(dbRef, 'pesajes/' +_user)).then((snapshot)=> {
+
+      if(snapshot.exists()) {
+        const resultadoPeticion = snapshot.val();
+        console.log("Este usuario ya tiene un pesaje. Añadiendo pesaje...");
+        console.log(resultadoPeticion);
+
+        const idUnico = Date.now();
+
+        const nuevoPesaje = {
+          [idUnico]: {
+            username: _user, 
+            fechaPesaje: _fechaPesaje, 
+            peso: _peso, 
+            imc: _imc,
+            metabolismoBasal: _metabolismoBasal,
+            grasa: _grasa,
+            masaMuscular: _masaMuscular,
+            porcentajeAgua: _porcentajeAgua
+          }
+        }
+
+        const nuevoObjeto = {...resultadoPeticion, ...nuevoPesaje};
+        console.log(nuevoObjeto);
+        set(ref(db, 'pesajes/' +_user), nuevoObjeto);
+      }
+      else {
+        console.log("Este usuario no tiene asignado ningun pesaje. Creando pesaje...")
+        const idUnico = Date.now();
+
+        const nuevoPesaje = {
+          [idUnico]: {
+            username: _user, 
+            fechaPesaje: _fechaPesaje, 
+            peso: _peso, 
+            imc: _imc,
+            metabolismoBasal: _metabolismoBasal,
+            grasa: _grasa,
+            masaMuscular: _masaMuscular,
+            porcentajeAgua: _porcentajeAgua
+          }
+        }
+        set(ref(db, 'pesajes/' +_user), nuevoPesaje);
+
+      }
+
+    }).catch((error)=> {
+      console.error(error);
+    });
+  }
 
   logout(){
     window.localStorage.clear();
