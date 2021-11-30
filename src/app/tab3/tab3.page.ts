140a39b04b5f29d958820f32053452814052b714
Antonio Jesús <i82ropia@uco.es>
2021-11-30 16:48:35 +0100

    Elegir rutina

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get, child, set } from 'firebase/database';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +11,72 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+ mensajeError: String ;
+ nombreRutina: any = [] ;
+  rutina: String;
+  ListaEjercicios: any = [];
+  ejercicio: string;
+  NRutina: string;
+
+
+ 
 
   constructor() {}
 
+
+  ShowRoutines(){
+    const app = initializeApp(environment.firebase);
+    const db = getDatabase(app);
+    const dbRef = ref(getDatabase(app));
+    const referencia= get(child(dbRef, 'rutinas/')).then((snapshot)=> {
+
+      if (snapshot.exists())
+      {
+        const resultadoPeticion = snapshot.val();
+        console.log(resultadoPeticion);
+        for (const i in resultadoPeticion){
+       
+             this.nombreRutina.push(resultadoPeticion[i].nombreRutina)
+           }
+          console.log(this.nombreRutina) 
+          this.mensajeError = 'Esta rutina no existe en nuestro plan';
+      }
+  }).catch((error) => {
+    console.error(error);
+  
+  });
+
+   this.nombreRutina = []};
+
+
+mostrarRutina(){
+
+  const app = initializeApp(environment.firebase);
+  const db = getDatabase(app);
+  const dbRef = ref(getDatabase(app));
+  const referencia= get(child(dbRef, 'rutinas/')).then((snapshot)=> {
+
+    if (snapshot.exists())
+    {
+      const resultadoPeticion = snapshot.val();
+      console.log(resultadoPeticion);
+      for (const i in resultadoPeticion){
+       console.log (this.NRutina)
+        if(this.NRutina === resultadoPeticion[i].nombreRutina)
+        {
+          this.ListaEjercicios=resultadoPeticion[i].ejercicios
+        }          
+      }
+      
+      console.log(this.ListaEjercicios);
+      this.mensajeError = 'Esta rutina no existe en nuestro plan';
+    }
+    
+
+}).catch((error) => {
+  console.error(error);
+
+});
+this.ListaEjercicios = [];
+}
 }
